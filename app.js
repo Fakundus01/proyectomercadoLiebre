06a17ed6a202fa95d25680a70b6849d707973250
Fakundus01 <facumoreno2001@gmail.com>
2021-05-27 20:56:47 -0300

    heroku

--- app.js
+++ app.js
@@ -8,9 +8,7 @@ const app=express();
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.set("port",process.env.PORT || 3000)
-
-app.listen(3000,()=>console.log("Server Start in http://localhost:3000"));
+app.listen(process.env.PORT || 3000,()=>console.log("Server Start in http://localhost:3000"));
 
 app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
 
