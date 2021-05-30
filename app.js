bed9d368ab27bcdeaade4f60f1ed146c1e37fdb3
Fakundus01 <facumoreno2001@gmail.com>
2021-05-30 17:10:37 -0300

    act

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const app=express();
 
 app.use(express.static(path.resolve(__dirname,"./public")));
 
-app.listen(process.env.PORT || 3000,()=>console.log("Server Start in http://localhost:3000"));
+app.listen(process.env.PORT || 3000,()=>console.log("Server Start in https://proyectomercadoliebre.herokuapp.com/"));
 
 app.get("/",(req,res)=> res.sendFile(path.join(__dirname,"views","home.html")))
 
